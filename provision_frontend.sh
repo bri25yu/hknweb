3e08d5d2c76a444719176a3424e595582df6fb9f
bri25yu <bri25yu@gmail.com>
2020-11-16 00:48:42 -0800

    adding logic to ensure that entire file is downloaded before piping to bash

--- provision_frontend.sh
+++ provision_frontend.sh
@@ -1,5 +1,5 @@
 echo "Installing nvm"  # See https://github.com/nvm-sh/nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | tac | tac | bash
 source ~/.nvm/nvm.sh
 
 echo "Installing Node"
